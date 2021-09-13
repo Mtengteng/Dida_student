805d9d552c96a4f16470f06bae255329fc2a47ed
mateng <mateng@beiwaionline.com>
2021-09-13 09:42:56 +0800

    更新
    Signed-off-by: mateng <mateng@beiwaionline.com>

--- Student/Controllers/StudyVC/StudyVC.m
+++ Student/Controllers/StudyVC/StudyVC.m
@@ -6,9 +6,12 @@
 //
 
 #import "StudyVC.h"
+#import "SelectSubView.h"
+#import "SCDictModel.h"
 
 
 @interface StudyVC ()
+@property (nonatomic, strong) SelectSubView *subView;
 
 @end
 
@@ -24,4 +27,22 @@
     
 }
 
+#pragma mark - LazyLoad -
+- (SelectSubView *)subView
+{
+    if (!_subView) {
+        
+        NSArray *array = @[@"我的动态",@"我的数据",@"我的内容"];
+        NSMutableArray *infoArray = [[NSMutableArray alloc] init];
+       
+        for (NSInteger i = 0; i < array.count; i++) {
+            SCDictInfoModel *model = [[SCDictInfoModel alloc] init];
+            model.dictValue = [array safeObjectAtIndex:i];
+            [infoArray addObject:model];
+        }
+        
+        _subView = [[SelectSubView alloc] initWithItemArray:infoArray];
+    }
+    return _subView;
+}
 @end
